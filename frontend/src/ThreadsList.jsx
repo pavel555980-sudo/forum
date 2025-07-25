7a091cd30d4326b82766ea12f9b49bac1c413e9c
pavel555980 <sundayer10@gmail.com>
2025-07-25 17:14:08 +0300

    add thread adding

--- frontend/src/ThreadsList.jsx
+++ frontend/src/ThreadsList.jsx
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 const QuestionsList = () => {
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
-    fetch('https://localhost:8000/api/v1/questions')
+    fetch('http://localhost:8000/api/v1/questions')
       .then((res) => {
         return res.json();
       })
